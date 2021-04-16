322fe80ce4928c8f365f26e3d28d0f266f83b948
channingc177 <channingc177@gmail.com>
2021-04-16 08:17:45 -0700

    functioning release

---
+++ work_4-16-21.py
@@ -0,0 +1,24 @@
+# class Rectangle:
+#     def __init__(self, in_wid = 1, in_hei = 2):
+#         self.width, self.height = in_wid, in_hei
+#
+#     def get_area(self):
+#         return round(self.width * self.height, 2)
+#
+#     def get_perimeter(self):
+#         return (2 * self.width) + (2 * self.height)
+#
+#     def print_vars(self):
+#         print(str(self.width), str(self.height), sep="\n")
+#
+#
+# def Main():
+#     rects = [Rectangle(4, 40), Rectangle(3.5, 35.7)]
+#     for i in range(0, len(rects)):
+#         rects[i].print_vars()
+#         print(str(rects[i].get_area()), str(rects[i].get_perimeter()), \
+#               sep="\n")
+#
+#
+# Main()
+# ----------------------------------------------------------------------
